10cf06f8fa80ad2711545b2bb6f243359e8f3b71
Lengendhs <166408647+Lengendhs@users.noreply.github.com>
2024-04-10 11:19:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6701944692:AAEv1Tf_gGXihfwke1U7B-iRvkh5uR9p-aU")
     API_ID = int(os.environ.get("API_ID", "28127959"))
     API_HASH = os.environ.get("API_HASH", "0debc9c74510961fc8cf134a3c6eae0f")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "6370953390")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "tel:6813146748")
